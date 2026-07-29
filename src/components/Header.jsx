d682d1e730835c6514e8526a892eaa085ddc3a38
yomnaelsafty <140314499+yomnaelsafty@users.noreply.github.com>
2026-07-30 01:16:07 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,22 @@ import { Link } from "react-router-dom";
 
 function Header({ setSearchTerm }) {
   const onSearchChange = (e) => {
-    e.preventDefault();
     setSearchTerm(e.target.value);
   };
 
   return (
-    <header className="bg-white text-black border-none ">
-      <div className="max-w-6xl mx-auto px-16 py-4 flex items-center justify-between gap-10 mb-2">
-        <Logo />
-        <div>
-          <Link
-            to="/"
-            className="text-[#3B3C4A] text-base font-normal transition-colors duration-200 hover:text-[#4B6BFB]"
-          >
-            Home
-          </Link>
+    <header className="bg-white text-black border-none">
+      <div className="max-w-6xl mx-auto px-4 sm:px-8 lg:px-16 py-4 flex flex-col sm:flex-row items-center justify-between gap-4 sm:gap-10 mb-2">
+        <div className="flex items-center justify-between w-full sm:w-auto gap-6">
+          <Logo />
         </div>
+        <Link
+          to="/"
+          className="text-[#3B3C4A] text-base font-normal transition-colors duration-200 hover:text-[#4B6BFB]"
+        >
+          Home
+        </Link>
+
         <SearchInput onChange={onSearchChange} />
       </div>
     </header>
