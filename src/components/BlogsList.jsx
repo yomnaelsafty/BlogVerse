34260d39f27f947c100cb42566e6de3c340e3c67
yomnaelsafty <140314499+yomnaelsafty@users.noreply.github.com>
2026-07-30 01:17:14 +0300

    Update BlogsList.jsx

--- src/components/BlogsList.jsx
+++ src/components/BlogsList.jsx
@@ -15,7 +15,7 @@ function BlogsList({ blogs, loading, error, setPage }) {
 
   return (
     <div className="max-w-6xl mx-auto px-6 py-8 flex flex-col gap-8">
-      <div className="grid grid-cols-3 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {blogs.map((blog) => (
           <BlogCard
             key={blog.id}
